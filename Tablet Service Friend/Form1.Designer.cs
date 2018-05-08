09448c63fc6cc19aa22a7896c969ec31cb8010ff
Tibi Dragoj <tibi660@gmail.com>
2018-05-08 21:04:26 +0200

    Restarts the wacom tablet service, plus a few other goodies

---
+++ Tablet Service Friend/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Tablet_Service_Friend
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restoreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extrasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wacomSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wacomPreferenceFileUtilityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.downloadWacomDriverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.changeTabletOrientationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(76, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Restart service";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.extrasToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(248, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.TabStop = true;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backupToolStripMenuItem,
+            this.restoreToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.fileToolStripMenuItem.Text = "Options";
+            // 
+            // backupToolStripMenuItem
+            // 
+            this.backupToolStripMenuItem.Name = "backupToolStripMenuItem";
+            this.backupToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.backupToolStripMenuItem.Text = "Backup";
+            this.backupToolStripMenuItem.Click += new System.EventHandler(this.backupToolStripMenuItem_Click);
+            // 
+            // restoreToolStripMenuItem
+            // 
+            this.restoreToolStripMenuItem.Name = "restoreToolStripMenuItem";
+            this.restoreToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.restoreToolStripMenuItem.Text = "Restore";
+            this.restoreToolStripMenuItem.Click += new System.EventHandler(this.restoreToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // extrasToolStripMenuItem
+            // 
+            this.extrasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wacomSettingsToolStripMenuItem,
+            this.wacomPreferenceFileUtilityToolStripMenuItem,
+            this.downloadWacomDriverToolStripMenuItem,
+            this.changeTabletOrientationToolStripMenuItem});
+            this.extrasToolStripMenuItem.Name = "extrasToolStripMenuItem";
+            this.extrasToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.extrasToolStripMenuItem.Text = "Extras";
+            // 
+            // wacomSettingsToolStripMenuItem
+            // 
+            this.wacomSettingsToolStripMenuItem.Name = "wacomSettingsToolStripMenuItem";
+            this.wacomSettingsToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.wacomSettingsToolStripMenuItem.Text = "Wacom settings";
+            this.wacomSettingsToolStripMenuItem.Click += new System.EventHandler(this.wacomSettingsToolStripMenuItem_Click);
+            // 
+            // wacomPreferenceFileUtilityToolStripMenuItem
+            // 
+            this.wacomPreferenceFileUtilityToolStripMenuItem.Name = "wacomPreferenceFileUtilityToolStripMenuItem";
+            this.wacomPreferenceFileUtilityToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.wacomPreferenceFileUtilityToolStripMenuItem.Text = "Wacom Preference File Utility";
+            this.wacomPreferenceFileUtilityToolStripMenuItem.Click += new System.EventHandler(this.wacomPreferenceFileUtilityToolStripMenuItem_Click);
+            // 
+            // downloadWacomDriverToolStripMenuItem
+            // 
+            this.downloadWacomDriverToolStripMenuItem.Name = "downloadWacomDriverToolStripMenuItem";
+            this.downloadWacomDriverToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.downloadWacomDriverToolStripMenuItem.Text = "Download Wacom driver";
+            this.downloadWacomDriverToolStripMenuItem.Click += new System.EventHandler(this.downloadWacomDriverToolStripMenuItem_Click);
+            // 
+            // changeTabletOrientationToolStripMenuItem
+            // 
+            this.changeTabletOrientationToolStripMenuItem.Name = "changeTabletOrientationToolStripMenuItem";
+            this.changeTabletOrientationToolStripMenuItem.Size = new System.Drawing.Size(253, 22);
+            this.changeTabletOrientationToolStripMenuItem.Text = "Change tablet orientation (TODO)";
+            this.changeTabletOrientationToolStripMenuItem.Visible = false;
+            this.changeTabletOrientationToolStripMenuItem.Click += new System.EventHandler(this.changeTabletOrientationToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.richTextBox1.ForeColor = System.Drawing.Color.DarkTurquoise;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(224, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(248, 207);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "mainWindow";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Tablet friend";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restoreToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extrasToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem wacomPreferenceFileUtilityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem downloadWacomDriverToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStripMenuItem wacomSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem changeTabletOrientationToolStripMenuItem;
+    }
+}
+
---
+++ Tablet Service Friend/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.IO;
+
+namespace Tablet_Service_Friend
+{
+    public partial class mainWindow : Form
+    {
+        string prefUtil { get; set; }
+        string wacomCPL { get; set; }
+        string tabletDat { get; set; }
+        string bekapFajl { get; set; }
+        string servis { get; set; }
+
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Verify if a service exists
+        public bool servisPostoji(string ServisIme)
+        {
+            return ServiceController.GetServices().Any(serviceController => serviceController.ServiceName.Equals(ServisIme));
+        }
+
+        //add a line to the textbox
+        public void ispisiTekst(string tekst)
+        {
+            richTextBox1.AppendText(tekst + Environment.NewLine);
+        }
+
+        //show error and disable almost all options if the driver isnt found on the system
+        public void nemaDrajvera(string tekst)
+        {
+            MessageBox.Show(String.Format("Wacom {0} not found. Please update or reinstall the driver.", tekst),
+                            "Driver missing",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+
+            button1.Enabled = false;
+            backupToolStripMenuItem.Enabled = false;
+            restoreToolStripMenuItem.Enabled = false;
+            wacomSettingsToolStripMenuItem.Enabled = false;
+            wacomPreferenceFileUtilityToolStripMenuItem.Enabled = false;
+            changeTabletOrientationToolStripMenuItem.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string progFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            string appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            //check whether the driver installed is "Consumer" (5.x.x.x) or "Professional" (6.x.x.x)
+            if (servisPostoji("WTabletServicePro"))
+             {
+                //find some essential driver files. NEEDS SOLUTION FOR 32-BIT SYSTEMS
+                wacomCPL = progFiles + "/Tablet/Wacom/Professional_CPL.exe";
+                prefUtil = progFiles + "/Tablet/Wacom/32/PrefUtil.exe";
+                tabletDat = appData + "/WTablet/Wacom_Tablet.dat";
+                bekapFajl = "Wacom_Tablet.dat";
+                servis = "WTabletServicePro";
+            }
+            else if (servisPostoji("WTabletServiceCon"))
+            {
+                wacomCPL = progFiles + "/Tablet/Pen/Consumer_CPL.exe";
+                prefUtil = progFiles + "/Tablet/Pen/32/PrefUtil.exe";
+                tabletDat = appData + "/WTablet/Pen_Tablet.dat";
+                bekapFajl = "Pen_Tablet.dat";
+                servis = "WTabletServiceCon";
+            }
+            else
+            {
+                nemaDrajvera("service");
+            }
+
+            //check if those essential files actually exist
+            if (File.Exists(prefUtil) && File.Exists(wacomCPL))
+            {
+                ispisiTekst("Ready");  ///add nicer text
+            }
+            else
+            {
+                nemaDrajvera("driver files");
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceController wacomService = new ServiceController(servis);            
+            //ispisiTekst(wacomService.Status.ToString());
+
+            try
+            {
+                this.Enabled = false; //disable form while waiting
+
+                //backup the settings first
+                var bekap = Path.GetTempFileName();
+                File.Copy(tabletDat, bekap, true);
+                //ispisiTekst("backed up");
+                
+                TimeSpan timeout = TimeSpan.FromSeconds(5);
+
+                //if the service is still running, stop it
+                if (wacomService.Status == ServiceControllerStatus.Running)
+                {
+                    wacomService.Stop();
+                    wacomService.WaitForStatus(ServiceControllerStatus.Stopped, timeout);
+                    //ispisiTekst("Service stopped");
+                }
+                
+                //now start it
+                wacomService.Start();
+                ispisiTekst("Restarting service...");
+                wacomService.WaitForStatus(ServiceControllerStatus.Running, timeout);
+
+                //restore the settings
+                File.Copy(bekap, tabletDat, true);
+                File.Delete(bekap);
+
+                ispisiTekst("Success! Please relaunch your painting app to get pressure sensitivity back.");
+
+                this.Enabled = true;
+            }
+            catch
+            {
+                richTextBox1.SelectionColor = Color.Red;
+                ispisiTekst("Could not restart service.");
+                this.Enabled = true;
+            }
+        }
+
+        private void backupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog bekap = new SaveFileDialog
+            {
+                Title = "Backup Tablet Settings",
+                FileName = bekapFajl,
+                Filter = "Wacom Configuration File|*.dat"
+            };  // make a dialog
+
+            if (bekap.ShowDialog() == DialogResult.OK)
+            {
+                File.Copy(tabletDat, bekap.FileName, true);
+                ispisiTekst("Settings backed up.");
+            }
+        }
+
+        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ristor = new OpenFileDialog
+            {
+                Title = "Restore Tablet Settings",
+                FileName = bekapFajl,
+                Filter = "Wacom Configuration File|*.dat"
+            };  // make a dialog
+
+            if (ristor.ShowDialog() == DialogResult.OK)
+            {
+                File.Copy(tabletDat, tabletDat+".bak", true);  // make a backup copy first
+                File.Copy(ristor.FileName, tabletDat, true);
+                ispisiTekst("Settings restored. A backup copy was made");
+            }            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void wacomSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(wacomCPL);
+        }
+
+        private void wacomPreferenceFileUtilityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(prefUtil);
+        }
+
+        private void downloadWacomDriverToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.wacom.com/en-us/support/product-support/drivers");
+        }
+
+        private void changeTabletOrientationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //to be added later
+            /*Form2 orientationDialog = new Form2();
+            orientationDialog.ShowDialog(); */
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        //close on escape key
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
